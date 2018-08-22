416001a39ba55f51e869c4a171215f0e81987c85
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-22 21:40:56 +0100

    added menu for phone and desktop, side nav slider and header markup and styles, added media query to check for screen of maxwidth 1024px

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 import '../scss/main.scss';
+const container = document.getElementById('container');
+const sideNavCheck = document.getElementById('container__sidenav-check');
+const sideNavBtn = document.getElementById('container__sidenav-btndiv');
+
+sideNavBtn.addEventListener('click', function () {
+  sideNavCheck.checked = true;
+})
+
+container.addEventListener('click', function () {
+  sideNavCheck.checked = false;
+})
