59b915d8f2077f869face1e1e92659bed698f3e7
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-28 23:05:05 +0100

    added some slider functionality

--- src/js/models/slider.js
+++ src/js/models/slider.js
@@ -2,23 +2,57 @@ export default class Slider {
   constructor(id, transitionVal) {
     this.id = id;
     this.transitionVal = transitionVal;
+    this.cardsContainer = document.getElementById(this.id);
+    this.btns = this.cardsContainer.nextElementSibling.children[0];
+    this.idTracker = 0;
   }
 
   initialize() {
-    const cardsContainer = document.getElementById(this.id);
-    const btns = cardsContainer.nextElementSibling.children[0];
-    const btnsArray = Array.from(btns.children);
+    if (window.matchMedia('(max-width: 800px)').matches) {
+      return this.smallerScreenInit();
+    }
+
+    if (window.matchMedia('(max-width: 998px)').matches) {
+      return this.smallScreenInit();
+    }
+
+    const cardsSlide = this.cardsContainer;
+    const btnsArray = Array.from(this.btns.children);
     const percentage = this.transitionVal;
-    let idTracker = 0;
+
     btnsArray.forEach(btn => {
-      btn.dataset.id = idTracker;
-      btn.id = `slider-btn-${idTracker++}`;
+      btn.dataset.id = this.idTracker;
+      btn.id = `slider-btn-${this.idTracker++}`;
       btn.addEventListener('click', function () {
-        cardsContainer.style.transform = `translateX(-${percentage * this.dataset.id}%)`;
+        cardsSlide.style.transform = `translateX(-${percentage * this.dataset.id}%)`;
         btnsArray.forEach(btn => {
           btn.dataset.id === this.dataset.id ? btn.style.background = `rgba(255, 143, 0, 1.0)` : btn.style.background = `transparent`;
         })
       })
     })
+  }
+
+  smallScreenInit() {
+    const cardsSlide = this.cardsContainer;
+    const markup = `<li class="clients__btn-listitem"></li>`;
+
+    this.btns.insertAdjacentHTML(`beforeend`, markup);
+    const btnsArray = Array.from(this.btns.children);
+    const smallScreenPerc = this.transitionVal + 4;
+
+    btnsArray.forEach(btn => {
+      btn.dataset.id = this.idTracker;
+      btn.id = `slider-btn-${this.idTracker++}`;
+      btn.addEventListener('click', function () {
+        cardsSlide.style.transform = `translateX(-${smallScreenPerc * this.dataset.id}%)`;
+        btnsArray.forEach(btn => {
+          btn.dataset.id === this.dataset.id ? btn.style.background = `rgba(255, 143, 0, 1.0)` : btn.style.background = `transparent`;
+        })
+      })
+    })
+  }
+
+  smallerScreenInit() {
+
   }
 }
