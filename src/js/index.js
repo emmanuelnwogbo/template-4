4079b4efdbe2e0892ab2e1b09dfa44ecb0b71fd4
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-08-29 20:30:55 +0100

    added portfolio items switch functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 import Slider from './models/slider';
+import Switch from './models/switch';
+import img from '../imgs/project0.jpg';
 
 const container = document.getElementById('container');
 const sideNavCheck = document.getElementById('container__sidenav-check');
@@ -15,3 +17,58 @@ container.addEventListener('click', function () {
 
 const clientsSlider = new Slider('clients__content', 96);
 clientsSlider.initialize();
+
+const portfolioCards = [{
+    id: 'portfolio-1',
+    img: img,
+    category: 'branding',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-2',
+    img: img,
+    category: 'photography',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-3',
+    img: img,
+    category: 'seo',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-4',
+    img: img,
+    category: 'branding',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-5',
+    img: img,
+    category: 'webdevelopment',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-6',
+    img: img,
+    category: 'seo',
+    name: 'Project Caption'
+  },
+  {
+    id: 'portfolio-7',
+    img: img,
+    category: 'webdevelopment',
+    name: 'Project Caption'
+  }
+]
+
+const btnLabels = [
+  'all',
+  'webdevelopment',
+  'branding',
+  'photography',
+  'seo'
+];
+
+const portfolioSwitch = new Switch('portfolio__container', 'port_btns', 'port_cards');
+portfolioSwitch.initialize(btnLabels, portfolioCards);
---
+++ src/js/models/switch.js
@@ -0,0 +1,124 @@
+export default class Switch {
+  constructor(topPar, btnsPar, cardsPar) {
+    this.topPar = document.getElementById(topPar);
+    this.btnsPar = document.getElementById(btnsPar);
+    this.cardsPar = document.getElementById(cardsPar);
+  }
+
+  initialize(labels, cards) {
+    let markup;
+    labels.forEach(label => {
+      markup = `<li class="portfolio__container-listitem" data-itemid=${label}>${label}</li>`;
+      this.btnsPar.insertAdjacentHTML(`beforeend`, markup);
+    });
+
+    cards.forEach(card => {
+      markup = `
+        <div class="portfolio__card" data-itemid=${card.category} id="${card.id}">
+          <figure class="portfolio__card-figure">
+            <img src="${card.img}" alt="" class="portfolio__card-img">
+            <p class="portfolio__card-category">${card.category}</p>
+            <p class="portfolio__card-name">${card.name}</p>
+          </figure>
+      </div>`;
+      this.cardsPar.insertAdjacentHTML(`beforeend`, markup);
+    });
+
+    this.switcher('.portfolio__container-listitem', '.portfolio__card', cards);
+  }
+
+  switcher(btnClassName, cardClassName, cardsArr) {
+    const btns = document.querySelectorAll(btnClassName);
+    const cardsParent = this.cardsPar;
+    let cards;
+    let markup;
+    let currentBtn;
+
+    function triggerSwitch() {
+      cards = document.querySelectorAll(cardClassName);
+      cards.forEach(card => {
+        card.style.transform = `scale(0)`
+      })
+      setTimeout(function () {
+        while (cardsParent.firstChild) {
+          cardsParent.removeChild(cardsParent.firstChild);
+        }
+      }, 400);
+    }
+
+    function insertCard(itemid) {
+      setTimeout(() => {
+        cardsArr.forEach(card => {
+          if (card.category === itemid) {
+            markup = `
+              <div class="portfolio__card" data-itemid=${card.category} id="${card.id}">
+                <figure class="portfolio__card-figure">
+                  <img src="${card.img}" alt="" class="portfolio__card-img">
+                  <p class="portfolio__card-category">${card.category}</p>
+                  <p class="portfolio__card-name">${card.name}</p>
+                </figure>
+              </div>`;
+            cardsParent.insertAdjacentHTML(`beforeend`, markup);
+            cards = document.querySelectorAll(cardClassName);
+            cards.forEach(card => {
+              card.style.transform = `scale(0)`;
+              setTimeout(function () {
+                card.style.transform = `scale(1)`;
+              }, 50)
+            })
+          }
+        })
+      }, 400);
+    }
+
+    btns.forEach(btn => {
+      btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (currentBtn === this.dataset.itemid) {
+          return;
+        }
+        currentBtn = this.dataset.itemid
+        if (this.dataset.itemid === 'all') {
+          let markup;
+          let alreadyPresent = [];
+          let notPresent = [];
+          cards = document.querySelectorAll(cardClassName);
+          if (cards.length === cardsArr.length) {
+            return;
+          }
+          cards.forEach(card => {
+            alreadyPresent.push(card.id);
+          });
+
+          cardsArr.forEach(card => {
+            if (!alreadyPresent.includes(card.id)) {
+              notPresent.push(card.id);
+              markup = `
+              <div class="portfolio__card" data-itemid=${card.category} id="${card.id}">
+                <figure class="portfolio__card-figure">
+                  <img src="${card.img}" alt="" class="portfolio__card-img">
+                  <p class="portfolio__card-category">${card.category}</p>
+                  <p class="portfolio__card-name">${card.name}</p>
+                </figure>
+              </div>`;
+              cardsParent.insertAdjacentHTML(`beforeend`, markup);
+            }
+          });
+          cards = document.querySelectorAll(cardClassName);
+          cards.forEach(card => {
+            if (notPresent.includes(card.id)) {
+              card.style.transform = `scale(0)`;
+              setTimeout(function () {
+                card.style.transform = `scale(1)`;
+              }, 50);
+            }
+          })
+          return;
+        }
+
+        triggerSwitch();
+        insertCard(this.dataset.itemid);
+      })
+    })
+  }
+}
